ff3723c9e97792e7a68957a770cc79fb3a4ecd3a
Jonathan Galperin <jonathan@galperin.com>
2014-09-18 19:37:47 -0400

    Debugging upload.

--- upload.php
+++ upload.php
@@ -55,13 +55,11 @@ else if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_FILES["myFile"])) {
 
     $withoutExt = preg_replace('/\\.[^.\\s]{3,4}$/', '', $filename);
 
-    exec("javac -cp '/var/www/html/uploads' $filename 2>&1", $outputone);
+    exec("javac -cp 'var/www/html/uploads' $filename 2>&1", $outputone);
 
-    exec("java -cp '/var/www/html/uploads' $withoutExt 2>&1", $outputtwo);
+    exec("java -cp 'var/www/html/uploads' $withoutExt 2>&1", $outputtwo);
 
-    echo($outputone . "\n" . "<br />");
-    echo($outputtwo . "\n" . "<br />");
-
-
+    print_r($outputone . "\n" . "<br />");
+    print_r($outputtwo . "\n" . "<br />");
 
 }

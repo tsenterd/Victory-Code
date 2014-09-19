62a39bbe07e5a894230c32c6e705b65bfdf06c6e
Jonathan Galperin <jonathan@galperin.com>
2014-09-18 20:17:49 -0400

    Debugging upload.

--- upload.php
+++ upload.php
@@ -55,7 +55,7 @@ else if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_FILES["myFile"])) {
 
     $compile = exec("javac -cp '/var/www/html/uploads' *.java 2>&1", $output1);
 
-    $return = exec("java -cp '/var/www/html/uploads' $withoutExt 2>&1", $output);
+    //$return = exec("java -cp '/var/www/html/uploads' $withoutExt 2>&1", $output);
 
     print_r($name . "<br>");
 
@@ -63,6 +63,6 @@ else if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_FILES["myFile"])) {
 
     print_r ($compile . "<br>");
 
-    print_r ($return);
+    //print_r ($return);
 
 }

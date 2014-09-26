74830eaa1dc3b7232c32426215d8181f36de3e85
Jonathan Galperin <jonathan@galperin.com>
2014-09-26 17:24:55 -0400

    Database connection tests.

---
+++ db_connect.inc.php
@@ -0,0 +1,17 @@
+<?php
+
+//database connection settings
+
+define ("HOST", "54.68.67.189");
+
+define ("DB_USER", "root");
+
+define ("PASS", "root");
+
+define ("DB", "competition");
+
+$conn = mysqli_connect(HOST, DB_USER, PASS) or die("could not connect to database.");
+
+$db = mysqli_select_db($conn, DB) or die();
+
+?>
---
+++ login.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+
+</head>
+
+
+<body>
+
+<h1>Please log in</h1>
+
+<form>
+    <input type="email"><br>
+    <input type="password"><br>
+    <input type="submit" value="Login"/>
+</form>
+
+</body>
+
+
+</html>

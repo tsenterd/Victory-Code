2dda8a22b962ee8439b0c44376cfc2e6c1a55f40
Jonathan Galperin <jonathan@galperin.com>
2014-09-26 19:41:31 -0400

    Validation changes. again.

--- validation.functions.inc.php
+++ validation.functions.inc.php
@@ -5,37 +5,8 @@
 function valid_email($email)
 {
 
-    // First, we check that there's one @ symbol, and that the lengths are right
-    if (!preg_match("/^[^@]{1,64}@[^@]{1,255}$/", $email))
-    {
-        // Email invalid because wrong number of characters in one section, or wrong number of @ symbols.
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         return false;
-    }
-    // Split it into sections to make life easier
-    $email_array = explode("@", $email);
-    $local_array = explode(".", $email_array[0]);
-    for ($i = 0; $i < sizeof($local_array); $i++)
-    {
-        if (!preg_match("/^(([A-Za-z0-9!#$%&#038;'*+/=?^_`{|}~-][A-Za-z0-9!#$%&#038;'*+/=?^_`{|}~\.-]{0,63})|(\"[^(\\|\")]{0,62}\"))$/",
-            $local_array[$i]))
-        {
-            return false;
-        }
-    }
-    if (!preg_match("/^\[?[0-9\.]+\]?$/", $email_array[1]))
-    { // Check if domain is IP. If not, it should be valid domain name
-        $domain_array = explode(".", $email_array[1]);
-        if (sizeof($domain_array) < 2)
-        {
-            return false; // Not enough parts to domain
-        }
-        for ($i = 0; $i < sizeof($domain_array); $i++)
-        {
-            if (!preg_match("/^(([A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9])|([A-Za-z0-9]+))$/", $domain_array[$i]))
-            {
-                return false;
-            }
-        }
     }
     return true;
 }
@@ -59,7 +30,7 @@ function valid_username($username, $minlength = 3, $maxlength = 30)
         return false; //toshort
     }
 
-    $result = preg_match("/^[A-Za-z0-9_\-]+$/", $username); //only A-Z, a-z and 0-9 are allowed
+    $result = preg_match("/^[\w]$/", $username); //only A-Z, a-z and 0-9 are allowed
 
     if ($result)
     {
@@ -92,7 +63,7 @@ function valid_password($pass, $minlength = 6, $maxlength = 15)
         return false;
     }
 
-    $result = preg_match("/^[A-Za-z0-9_\-]+$/", $pass);
+    $result = preg_match("/^[\w]$/", $pass);
 
     if ($result)
     {

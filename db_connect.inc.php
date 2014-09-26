2422695e29a6fb70ba390b8686f3925870bdc30a
Jonathan Galperin <jonathan@galperin.com>
2014-09-26 18:30:33 -0400

    Fixed db connect.

--- db_connect.inc.php
+++ db_connect.inc.php
@@ -2,7 +2,7 @@
 
 //database connection settings
 
-define ("HOST", "localhost:3306");
+define ("HOST", "54.68.67.189");
 
 define ("DB_USER", "root");
 
@@ -12,8 +12,6 @@ define ("DB", "competition");
 
 global $conn;
 
-$conn = mysqli_connect(HOST, DB_USER, PASS) or die("could not connect to database.");
-
-$db = mysqli_select_db($conn, DB) or die();
+$conn = mysqli_connect(HOST, DB_USER, PASS, DB, "3306") or die("could not connect to database.");
 
 ?>

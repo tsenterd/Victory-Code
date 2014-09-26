493e1ae4c6381c670b46519fc30cbf9d1dba0b6a
Jonathan Galperin <jonathan@galperin.com>
2014-09-26 19:12:48 -0400

    Debugging.

--- db_connect.inc.php
+++ db_connect.inc.php
@@ -2,7 +2,7 @@
 
 //database connection settings
 
-define ("HOST", "localhost");
+define ("HOST", "localhost:3306");
 
 define ("DB_USER", "root");
 
@@ -12,6 +12,8 @@ define ("DB", "competition");
 
 $seed = "0dAfghRqSTgx";
 
-$conn = mysqli_connect(HOST, DB_USER, PASS, DB, "3306") or die("could not connect to database.");
+$conn = mysqli_connect(HOST, DB_USER, PASS) or die("could not connect to database.");
+
+$database = mysqli_select_db($conn, DB);
 
 ?>

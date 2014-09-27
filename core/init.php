656e061acad8d9704b82da0de460b1807920dcf4
Jonathan Galperin <jonathan@galperin.com>
2014-09-27 14:42:42 -0400

    Testing database functions.

--- core/init.php
+++ core/init.php
@@ -22,6 +22,7 @@ $GLOBALS['config'] = array(
 
 spl_autoload_register(function($class) {
     require_once '../classes/' . $class . '.php';
+    //require_once '../classes/Config.php';
 });
 
-require_once 'functions/sanitize.php';
+require_once '../functions/sanitize.php';

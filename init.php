0a605ed8c4e285c74e681043f68e08b9dbd9d8f2
Jonathan Galperin <jonathan@galperin.com>
2014-09-27 17:00:37 -0400

    open stream fix.

--- init.php
+++ init.php
@@ -21,7 +21,7 @@ $GLOBALS['config'] = array(
 );
 
 spl_autoload_register(function($class) {
-    require_once ('classes/' . $class . '.php');
+    require_once ('html/classes/' . $class . '.php');
 });
 
-require_once('../functions/sanitize.php');
+require_once('html/functions/sanitize.php');

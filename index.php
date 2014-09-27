9e918b6df9d8e61bcabd3c51ee55413c148c7e48
Jonathan Galperin <jonathan@galperin.com>
2014-09-27 10:38:16 -0400

    Massive refactoring.

--- index.php
+++ index.php
@@ -21,9 +21,11 @@
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
 
-        <?php
 
-        include "login.php";
+        <?php
+            require_once 'core/init.php';
+
+            DB::getInstance();
 
         ?>
 

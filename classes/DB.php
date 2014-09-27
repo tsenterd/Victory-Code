f1aced05cfb83ba8c50891481faa8a9e437e6f10
Jonathan Galperin <jonathan@galperin.com>
2014-09-27 14:12:01 -0400

    Testing database functions.

--- classes/DB.php
+++ classes/DB.php
@@ -13,6 +13,7 @@ class DB {
     private function __construct() {
         echo "hit construct.";
         try {
+            echo "before try.";
             $this->_pdo = new PDO('mysql:host='.Config::get('mysql/host').';port=3306;dbname='.Config::get('mysql/db'), Config::get('mysql/username'), Config::get('mysql/password'));
             echo "after try.";
         } catch (PDOException $e) {

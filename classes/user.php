4e905f780ca8a8321bba4dca2dec5ab4ee26228e
Jonathan Galperin <jonathan@galperin.com>
2014-09-30 21:33:54 -0400

    file name change?

--- classes/user.php
+++ classes/user.php
@@ -1,7 +1,8 @@
 <?php
 
 class User {
-    private $_db;
+    private $_db,
+            $_data;
 
     public function __construct($user = null) {
         $this ->_db = DB::getInstance();
@@ -11,5 +12,23 @@ class User {
         if ($this->_db->insert('users', $fields)) {
             throw new Exception('There was a problem creating an account.');
         }
+    }
+
+    public function find ($user = null) {
+        if ($user) {
+            $data = $this->_db->get('users', array('username', '=', $user));
+
+            if ($data->count()) {
+                $this->_data = $data->first();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function login ($username = null, $password = null) {
+        $user = $this->find($username);
+        print_r($this->_data);
+        return false;
     }
 }

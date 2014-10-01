e8f2594795e1ec711fd51c93ccf025674cf5eb3a
Jonathan Galperin <jonathan@galperin.com>
2014-09-30 21:37:17 -0400

    User login.

--- classes/DB.php
+++ classes/DB.php
@@ -129,6 +129,10 @@ class DB {
         return $this->_results;
     }
 
+    public function first() {
+        return $this->results()[0];
+    }
+
     public function error() {
         return $this->_error;
     }

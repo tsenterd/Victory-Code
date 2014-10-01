3b6bae0d7c2fd1167d5278ab6e18474d104b6510
Jonathan Galperin <jonathan@galperin.com>
2014-09-30 20:37:30 -0400

    user registration.

--- register.php
+++ register.php
@@ -44,24 +44,25 @@
 ?>
 
 <form action="" method="post">
-        <div class="field">
-            <label for="username">Username</label>
-            <input type="text" name="username" id="username" value="<?php echo escape(Input::get('username'));?>" autocomplete="off">
-        </div>
+    <div class="field">
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username" value="<?php echo escape(Input::get('username')); ?>"
+               autocomplete="off">
+    </div>
 
-        <div class="field">
-            <label for="password">Choose a password</label>
-            <input type="password" name="password" id="password">
-        </div>
+    <div class="field">
+        <label for="password">Choose a password</label>
+        <input type="password" name="password" id="password">
+    </div>
 
     <div class="field">
         <label for="password_again">Enter password again</label>
-        <input type="password" name="password" id="password_again">
+        <input type="password" name="password_again" id="password_again">
     </div>
 
     <div class="field">
         <label for="name">Enter your name</label>
-        <input type="text" name="name" value="<?php echo escape(Input::get('name'));?>" id="name">
+        <input type="text" name="name" value="<?php echo escape(Input::get('name')); ?>" id="name">
     </div>
 
     <input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
